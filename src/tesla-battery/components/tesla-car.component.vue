ee9a70fd6fc46dfa6f69511c8d45d164ff923492
Victor <victor@hackages.io>
2018-03-20 09:26:21 +0100

    fix(tesla): optimization

--- src/tesla-battery/components/tesla-car.component.vue
+++ src/tesla-battery/components/tesla-car.component.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="tesla-car">
         <div class="tesla-wheels">
-            <div :class="`tesla-wheel tesla-wheel--front tesla-wheel--${wheelsize}--${ speed }`"></div>
+            <div :class="`tesla-wheel tesla-wheel--front tesla-wheel--${wheelsize}--${speed}`"></div>
             <div :class="`tesla-wheel tesla-wheel--rear tesla-wheel--${wheelsize}--${speed}`"></div>
         </div>
     </div>
--- src/tesla-battery/components/tesla-wheels.component.vue
+++ src/tesla-battery/components/tesla-wheels.component.vue
@@ -1,30 +1,24 @@
 <template>
-    <div class="tesla-wheels">
-        <p class="tesla-wheels__title">Wheels</p>
-        <div class="tesla-wheels__container cf">
-            <label v-for="size in sizes" :key="size" :class="[{'tesla-wheels__item--active' : (value === size), 'tesla-wheels__item--focused': (focused === size),'tesla-wheels__item': true}, `tesla-wheels__item--${size}`]">
-                <input type="radio" name="wheelsize" :value="size" @blur="onBlur()" @click="onClick(size)" @focus="onFocus(size)" :checked="value === size">
-                <p>
-                    {{ size }}"
-                </p>
-            </label>
-        </div>
+  <div class="tesla-wheels">
+    <p class="tesla-wheels__title">Wheels</p>
+    <div class="tesla-wheels__container cf">
+      <label v-for="size in sizes" :key="size" :class="[{'tesla-wheels__item--active' : (value === size), 'tesla-wheels__item--focused': (focused === size),'tesla-wheels__item': true}, `tesla-wheels__item--${size}`]">
+        <input type="radio" name="wheelsize" :value="size" @blur="onBlur" @click="$emit('input', size)" @focus="onFocus(size)" :checked="value === size">
+        <p>
+          {{ size }}"
+        </p>
+      </label>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'tesla-wheels',
-  props: {
-    onClick: {
-      type: Function,
-      required: true,
-    },
-  },
+  props: ['value'],
   data() {
     return {
       sizes: [19, 21],
-      value: null,
       focused: null,
     };
   },
